5480f33b18d86804d11dbe7bb506c453624df048
klavoraMate <klavoramate98@gmail.com>
2023-10-21 12:35:01 +0200

    feat: added rest of the types

---
+++ application/src/data/DistanceBearing.ts
@@ -0,0 +1,7 @@
+import {Point} from "./Point";
+
+export type DistanceBearing={
+    point:Point
+    distance:number,
+    bearing:number
+}
---
+++ application/src/data/GPSTrack.ts
@@ -0,0 +1,7 @@
+import {Point} from "./Point";
+
+export type GPSTrack = {
+    date:string,
+    time:string,
+    point:Point
+}
