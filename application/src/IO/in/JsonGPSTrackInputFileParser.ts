5718b76b1c07c2a73076ac0d399024b3f11b87ce
klavoraMate <klavoramate98@gmail.com>
2023-10-21 17:13:29 +0200

    fix: fixed method signature of parser implementation

--- application/src/IO/in/JsonGPSTrackInputFileParser.ts
+++ application/src/IO/in/JsonGPSTrackInputFileParser.ts
@@ -4,8 +4,8 @@ import {GPSTrack} from "../../data/GPSTrack";
 export class JsonGPSTrackInputFileParser implements GenericGPSTrackInputFileParser {
     private fileName: string;
 
-    parse(): GPSTrack {
-        return {point:{lat: 0, lon: 0}, date: "0", time: "0"};
+    parse(): GPSTrack[] {
+        return [{point:{lat: 0, lon: 0}, date: "0", time: "0"}];
     }
 
     constructor(inputFileName: string) {
