d1bc472e385e3c71db6dce853a0a72e25c175111
klavoraMate <klavoramate98@gmail.com>
2023-10-21 13:58:19 +0200

    feat: created logic which handles args of the start command and creates calculator based on these parameters

--- application/src/app.ts
+++ application/src/app.ts
@@ -1,1 +1,28 @@
-console.log(process.argv)
+import {IODistanceBearingCalculator} from "./logic/IODistanceBearingCalculator";
+import {JsonGPSTrackInputFileParser} from "./IO/in/JsonGPSTrackInputFileParser";
+import {MetricDistanceBearingToJsonOutputGenerator} from "./IO/out/MetricDistanceBearingToJsonOutputGenerator";
+import {ImperialDistanceBearingToJsonOutputGenerator} from "./IO/out/ImperialDistanceBearingToJsonOutputGenerator";
+
+const args = process.argv.slice(2);
+const inputFileName = args[0];
+const outputFileName = args[1];
+const measurementSystem = args[2]
+switch (measurementSystem) {
+    //todo: create a IODistanceBearingCalculatorBuilder
+    case "metric": {
+        const parser = new JsonGPSTrackInputFileParser(inputFileName);
+        const generator = new MetricDistanceBearingToJsonOutputGenerator(outputFileName);
+        const calculator = new IODistanceBearingCalculator(parser, generator);
+        calculator.run();
+        break;
+    }
+    case "imperial": {
+        const parser = new JsonGPSTrackInputFileParser(inputFileName);
+        const generator = new ImperialDistanceBearingToJsonOutputGenerator(outputFileName);
+        const calculator = new IODistanceBearingCalculator(parser, generator);
+        calculator.run()
+        break;
+    }
+    default:
+        console.log("Invalid command. Usage: node ./dir/src/app.js <input file name> <output file name> [metric,imperial]")
+}
