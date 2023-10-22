52b0c32725006ccdd5d2203742c30f13b6e61f4d
klavoraMate <klavoramate98@gmail.com>
2023-10-22 20:42:31 +0200

    feat: implemented distance bearing calculator calculate method

--- application/src/logic/DistanceBearingCalculator.ts
+++ application/src/logic/DistanceBearingCalculator.ts
@@ -1,15 +1,22 @@
 import {Calculator} from "./Calculator";
 import {GPSTrack} from "../data/GPSTrack";
 import {DistanceBearing} from "../data/DistanceBearing";
+import {GeoUtils} from "../util/GeoUtils";
 
 export abstract class DistanceBearingCalculator extends Calculator<GPSTrack[], DistanceBearing[]> {
-    protected tracks: GPSTrack[] = [];
-    protected results: DistanceBearing[] = [];
-
-
-
-
     calculate(input: GPSTrack[]): DistanceBearing[] {
-        return [{point: {lat: 0, lon: 0}, distance: 0, bearing: 0}];
+        const result: DistanceBearing[] = [];
+        for (let i = 0; i < input.length - 1; i++) {
+            const pointA = input[i].GPSP;
+            const pointB = input[i + 1].GPSP;
+            const distance = GeoUtils.distance(pointA, pointB);
+            const bearing = GeoUtils.bearing(pointA, pointB)
+            result.push({
+                fromGPSP: pointA,
+                distance: distance,
+                bearing: bearing
+            })
+        }
+        return result;
     }
 }
