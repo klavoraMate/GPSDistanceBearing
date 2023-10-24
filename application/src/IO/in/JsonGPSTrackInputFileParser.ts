eac802efb3db9a57cf28a51d5db45af14142fd22
klavoraMate <klavoramate98@gmail.com>
2023-10-24 17:06:35 +0200

    feat: implemented different input file reading strategy
    I have choosed this different approach of file reading because when dealing
    with large files it would be impossible to read the whole file at once.
    This approach uses an asyncGenerator and reads with fs.readSync() method.
    This way the reading process is streamlined. At this point this approach is not tested with large input file.

--- application/src/IO/in/JsonGPSTrackInputFileParser.ts
+++ application/src/IO/in/JsonGPSTrackInputFileParser.ts
@@ -16,24 +16,77 @@ export class JsonGPSTrackInputFileParser implements GenericGPSTrackInputFilePars
 
     /**
      * Asynchronously parses a JSON file and returns an AsyncGenerator of GPSTrack objects.
+     * This method expects the input file to be in the following JSON format:
+     * {
+     *   "input": [
+     *     {
+     *       "date": "2013-06-01",
+     *       "time": "00:00:04",
+     *       "GPSP": {
+     *         "lat": 47.50741,
+     *         "lon": 18.60759
+     *       }
+     *     },
+     *     {
+     *       "date": "2013-06-01",
+     *       "time": "00:00:54",
+     *       "GPSP": {
+     *         "lat": 47.50060,
+     *         "lon": 18.59482
+     *       }
+     *     }
+     *   ]
+     * }*
+     *
      * @returns An AsyncGenerator of GPSTrack objects.
      */
     async* parse(): AsyncGenerator<GPSTrack> {
         const jsonFilePath = path.join(__dirname, '../../../../resources/' + this.fileName);
         const fileDescriptor = fs.openSync(jsonFilePath, 'r');
-        const fileStream = fs.createReadStream(jsonFilePath, {encoding: 'utf8'});
         const buffer = Buffer.alloc(1);
-        let jsonString = '';
-        let bytesRead;
 
-        while (bytesRead = fs.readSync(fileDescriptor, buffer, 0, 1, null) > 0) {
+        // remove trailing: {input:[
+        let endReached = false;
+        while (fs.readSync(fileDescriptor, buffer, 0, 1, null) > 0 && !endReached) {
             const char = buffer.toString();
+            if (char === '[')
+                endReached = true;
+        }
+
+        while (fs.readSync(fileDescriptor, buffer, 0, 1, null) > 0) {
+            endReached = false;
+            let closingCount = 0;
+            let jsonString = '';
+            while (fs.readSync(fileDescriptor, buffer, 0, 1, null) > 0 && !endReached) {
+                const char = buffer.toString();
+                jsonString += char;
+                if (char === '}')
+                    closingCount++;
+                if (closingCount == 2)
+                    endReached = true;
+            }
+
+            //try parsing only when jsonString contains the necessary values
+            if (jsonString.includes("date")&&jsonString.includes("time")&&jsonString.includes("GPSP")){
+                try {
+                    const jsonTrack = JSON.parse(jsonString);
+                    const track: GPSTrack = {
+                        date: jsonTrack.date,
+                        time: jsonTrack.time,
+                        GPSP: {
+                            lat: jsonTrack.GPSP.lat,
+                            lon: jsonTrack.GPSP.lon,
+                        },
+                    };
+                    yield track;
+                } catch (error) {
+                    Logger.error('Error parsing JSON:', error);
+                    throw error;
+                }
+            }
 
         }
 
-        if (jsonString.trim() !== '') {
-            Logger.info('Incomplete JSON data at the end of the file: ' + jsonString);
-        }
 
     }
 }
